3488151fff931e3bb10368f537c71bc21d9ed1d0
spatulatom <spatulatom@gmail.com>
2022-12-28 00:26:23 +0000

    firs commit

---
+++ store/notification-context.js
@@ -0,0 +1,52 @@
+import { createContext, useState, useEffect } from 'react';
+
+const NotificationContext = createContext({
+  notification: null, // { title, message, status }
+  showNotification: function (notificationData) {},
+  hideNotification: function () {},
+});
+
+export function NotificationContextProvider(props) {
+  const [activeNotification, setActiveNotification] = useState();
+
+  useEffect(() => {
+    if (
+      activeNotification &&
+      (activeNotification.status === 'success' ||
+        activeNotification.status === 'error')
+    ) {
+      const timer = setTimeout(() => {
+        setActiveNotification(null);
+      }, 3000);
+ 
+      // if useEffect reruns before the timer goes off
+      // to avoid multiple setTimeot running we do the 
+      // clen up with clearTimeout
+      return () => {
+        clearTimeout(timer);
+      };
+    }
+  }, [activeNotification]);
+
+  function showNotificationHandler(notificationData) {
+    setActiveNotification(notificationData);
+  }
+
+  function hideNotificationHandler() {
+    setActiveNotification(null);
+  }
+
+  const context = {
+    notification: activeNotification,
+    showNotification: showNotificationHandler,
+    hideNotification: hideNotificationHandler,
+  };
+
+  return (
+    <NotificationContext.Provider value={context}>
+      {props.children}
+    </NotificationContext.Provider>
+  );
+}
+
+export default NotificationContext;
